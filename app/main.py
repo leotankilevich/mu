ebe9bf452cf93de5cd2182351fbc79e029f91065
Leonid Tankilevich <leonid@republic.co>
2024-11-22 01:36:09 +0300

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes import utils
 from app.routes.graphql import graphql_app
@@ -20,6 +21,14 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
